6ff3b73ea244710a4e80d1023c6443c65709146d
Brandon Block <brandon.block@gmail.com>
2016-09-30 14:08:00 -0400

    handler sketch

--- main.go
+++ main.go
@@ -8,5 +8,18 @@ import (
 )
 
 func main() {
-	fmt.Println("vim-go")
+	r := httprouter.New()
+	r.GET("/", HomeHandler)
+
+	//Posts
+	r.GET("/posts", PostsIndexHandler)
+	r.POST("/posts", PostsCreateHandler)
+
+	//Post, individual
+	r.GET("/posts/:id", PostShowHandler)
+	r.PUT("/posts/:id", PostUpdateHandler)
+	r.GET("/posts/:id/edit", PostEditHandler)
+
+	fmt.Println("Starting server on :8008")
+	http.ListenAndServe(":8080", r)
 }
